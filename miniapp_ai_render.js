f1c1c969947b1d33ebd6628c4c56f9a231eb1883
Maladesbola <echiptacreator@gmail.com>
2026-01-22 00:34:43 +0500

    Create miniapp_ai_render.js

---
+++ miniapp_ai_render.js
@@ -0,0 +1,29 @@
+function buildPost(data) {
+  let text = "";
+
+  text += `🚕 ${data.from} → ${data.to}\n\n`;
+  text += `👥 ${data.people}\n`;
+  text += `⏰ ${data.time}\n`;
+
+  if (data.urgent === "Ha (tezkor)") {
+    text += `⚡ TEZKOR\n`;
+  }
+
+  if (data.female === "Ha") {
+    text += `👩 Ayol kishi bor\n`;
+  }
+
+  text += `🚗 Mashina: ${data.car} (${data.fuel})\n`;
+
+  if (data.package !== "Yo‘q") {
+    text += `📦 Pochta: ${data.package}\n`;
+  }
+
+  text += `\n📞 ${data.phone}`;
+
+  if (data.telegram) {
+    text += `\n💬 ${data.telegram}`;
+  }
+
+  return text;
+}
