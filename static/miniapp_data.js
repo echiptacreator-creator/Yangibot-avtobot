803710426eec63d77e9c1c3648f4cbce586f7bbc
Maladesbola <echiptacreator@gmail.com>
2026-01-22 19:33:35 +0500

    Update miniapp_data.js

--- static/miniapp_data.js
+++ static/miniapp_data.js
@@ -1,7 +1,205 @@
+// miniapp_data.js
+
 const REGIONS = {
-  Toshkent: ["Chilonzor","Yunusobod","Olmazor"],
-  Andijon: ["Asaka","Xo‘jaobod","Izboskan"],
-  Fargona: ["Marg‘ilon","Qo‘qon"]
+  "Andijon viloyati": [
+    "Andijon tumani",
+    "Asaka tumani",
+    "Baliqchi tumani",
+    "Bo‘ston tumani",
+    "Buloqboshi tumani",
+    "Izboskan tumani",
+    "Jalaquduq tumani",
+    "Xo‘jaobod tumani",
+    "Qo‘rg‘ontepa tumani",
+    "Marhamat tumani",
+    "Oltinko‘l tumani",
+    "Paxtaobod tumani",
+    "Shahrixon tumani",
+    "Ulug‘nor tumani"
+  ],
+
+  "Buxoro viloyati": [
+    "Buxoro tumani",
+    "G‘ijduvon tumani",
+    "Jondor tumani",
+    "Kogon tumani",
+    "Olot tumani",
+    "Qorako‘l tumani",
+    "Qorovulbozor tumani",
+    "Peshku tumani",
+    "Romitan tumani",
+    "Shofirkon tumani",
+    "Vobkent tumani"
+  ],
+
+  "Farg‘ona viloyati": [
+    "Bag‘dod tumani",
+    "Beshariq tumani",
+    "Buvayda tumani",
+    "Dang‘ara tumani",
+    "Farg‘ona tumani",
+    "Quva tumani",
+    "Rishton tumani",
+    "So‘x tumani",
+    "Toshloq tumani",
+    "Uchko‘prik tumani",
+    "Yozyovon tumani"
+  ],
+
+  "Jizzax viloyati": [
+    "Arnasoy tumani",
+    "Baxmal tumani",
+    "Forish tumani",
+    "G‘allaorol tumani",
+    "Zafarobod tumani",
+    "Zomin tumani",
+    "Do‘stlik tumani",
+    "Mirzacho‘l tumani",
+    "Paxtakor tumani",
+    "Yangiobod tumani",
+    "Yangiariq tumani",
+    "Yangiobod tumani"
+  ],
+
+  "Qashqadaryo viloyati": [
+    "Chiroqchi tumani",
+    "Dehqonobod tumani",
+    "G‘uzor tumani",
+    "Kitob tumani",
+    "Koson tumani",
+    "Mirishkor tumani",
+    "Qarshi tumani",
+    "Shahrisabz tumani",
+    "Yakkabog‘ tumani",
+    "Yangi Nishon tumani",
+    "Kasbi tumani",
+    "Muborak tumani",
+    "Nishon tumani"
+  ],
+
+  "Namangan viloyati": [
+    "Chortoq tumani",
+    "Kosonsoy tumani",
+    "Mingbuloq tumani",
+    "Namangan tumani",
+    "Norin tumani",
+    "Pop tumani",
+    "To‘raqo‘rg‘on tumani",
+    "Uchqo‘rg‘on tumani",
+    "Uychi tumani",
+    "Yangi Namangan tumani",
+    "Yangiqo‘rg‘on tumani"
+  ],
+
+  "Navoiy viloyati": [
+    "Karmana tumani",
+    "Konimex tumani",
+    "Navbahor tumani",
+    "Nurota tumani",
+    "Tomdi tumani",
+    "Uchquduq tumani",
+    "Xatirchi tumani",
+    "Zarbdor tumani"
+  ],
+
+  "Samarqand viloyati": [
+    "Bulung‘ur tumani",
+    "Ishtixon tumani",
+    "Jomboy tumani",
+    "Kattaqo‘rg‘on tumani",
+    "Narpay tumani",
+    "Nurobod tumani",
+    "Oqdaryo tumani",
+    "Paxtachi tumani",
+    "Pastdarg‘om tumani",
+    "Payariq tumani",
+    "Samarkand tumani",
+    "Toyloq tumani",
+    "Urgut tumani"
+  ],
+
+  "Surxondaryo viloyati": [
+    "Angor tumani",
+    "Boysun tumani",
+    "Denov tumani",
+    "Jarqo‘rg‘on tumani",
+    "Muzrabot tumani",
+    "Oltinsoy tumani",
+    "Qiziriq tumani",
+    "Sariosiyo tumani",
+    "Sherobod tumani",
+    "Sho‘rchi tumani",
+    "Termiz tumani"
+  ],
+
+  "Sirdaryo viloyati": [
+    "Boyovut tumani",
+    "Guliston tumani",
+    "Mirzaobod tumani",
+    "Oqoltin tumani",
+    "Sayxunobod tumani",
+    "Sirdaryo tumani",
+    "Xovos tumani"
+  ],
+
+  "Toshkent viloyati": [
+    "Bekobod tumani",
+    "Bo‘ka tumani",
+    "Bo‘stonliq tumani",
+    "Chinoz tumani",
+    "Ohangaron tumani",
+    "Oqqo‘rg‘on tumani",
+    "O‘rta Chirchiq tumani",
+    "Parkent tumani",
+    "Piskent tumani",
+    "Qibray tumani",
+    "Quyi Chirchiq tumani",
+    "Toshkent tumani",
+    "Yangiyo‘l tumani",
+    "Yuqori Chirchiq tumani",
+    "Zangiota tumani"
+  ],
+
+  "Xorazm viloyati": [
+    "Bog‘ot tumani",
+    "Gurlan tumani",
+    "Hazorasp tumani",
+    "Khiva tumani",
+    "Qo‘ng‘irot tumani",
+    "Shovot tumani",
+    "Urganch tumani",
+    "Xonqa tumani",
+    "Yangiariq tumani"
+  ],
+
+  "Qoraqalpog‘iston Respublikasi": [
+    "Amudaryo tumani",
+    "Beruniy tumani",
+    "Chimboy tumani",
+    "Ellikqal‘a tumani",
+    "Kechikli tumani",
+    "Mo‘ynoq tumani",
+    "Nukus tumani",
+    "Qanliko‘l tumani",
+    "Qo‘ng‘irot tumani",
+    "Taxtako‘pir tumani",
+    "To‘rtko‘l tumani"
+  ],
+
+  "Toshkent shahri": [
+    "Bektemir tumani",
+    "Chilonzor tumani",
+    "Mirzo Ulug‘bek tumani",
+    "Mirobod tumani",
+    "Sergeli tumani",
+    "Shayxontohur tumani",
+    "Uchtepa tumani",
+    "Yunusobod tumani",
+    "Yakkasaroy tumani",
+    "Yangihayot tumani",
+    "Olmazor tumani",
+    "Yashnobod tumani"
+  ]
 };
 
 const CARS = [
