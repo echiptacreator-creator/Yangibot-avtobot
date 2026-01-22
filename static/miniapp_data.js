983d4edf8945543d228d0d895c7ac0816c45ec2e
Maladesbola <echiptacreator@gmail.com>
2026-01-22 18:09:16 +0500

    Create miniapp_data.js

---
+++ static/miniapp_data.js
@@ -0,0 +1,9 @@
+const REGIONS = {
+  Toshkent: ["Chilonzor","Yunusobod","Olmazor"],
+  Andijon: ["Asaka","Xo‘jaobod","Izboskan"],
+  Fargona: ["Marg‘ilon","Qo‘qon"]
+};
+
+const CARS = [
+  "Gentra","Cobalt","Nexia 3","Tracker","Onix","BYD","Boshqa"
+];
